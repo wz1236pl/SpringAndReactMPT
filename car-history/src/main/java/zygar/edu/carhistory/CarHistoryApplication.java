450467c76bf737aa182e222552a686749b24e042
wz1236pl <dziq243@gmail.com>
2023-11-06 22:59:54 +0100

    init app

---
+++ car-history/src/main/java/zygar/edu/carhistory/CarHistoryApplication.java
@@ -0,0 +1,13 @@
+package zygar.edu.carhistory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarHistoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarHistoryApplication.class, args);
+	}
+
+}
---
+++ car-history/src/test/java/zygar/edu/carhistory/CarHistoryApplicationTests.java
@@ -0,0 +1,13 @@
+package zygar.edu.carhistory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarHistoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
